d40a6856c99c02dc67a9eb569e53a86a66694786
Andrew Hexen <SyberHexen@gmail.com>
2023-03-23 01:19:37 -0700

    AK3: Import TWRP Installer for AIO patching
    Notes: 
    Place twrp ramdisk in root folder with filename ramdisk-recovery.cpio
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=A/B Ramdisk lz4 remover for lahaina by SyberHexen @ xda-developers
+kernel.string=kernel.string=A/B Ramdisk lz4 remover for lahaina + TWRP installer by SyberHexen @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=0
